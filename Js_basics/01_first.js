7bf55c3e21b2a312c44819e077298e6085a05f20
Muhammad Afzal Hashmi <136243100+Afzal-Hashmi@users.noreply.github.com>
2024-04-09 21:41:49 +0000

    First commit learned variables datatypes

---
+++ Js_basics/01_first.js
@@ -0,0 +1,23 @@
+"use strict"; //using this means all Js code will be treated as newer version
+// alert(3+3) // this work in browser not in node js
+
+//variables
+const accountNo = 9000 // const scope is block level this value and variable can not be redefined of changed
+let emailId = "Afzal@gmail.com" // lets scope is also block but its value can be changed in future
+var phoneN = 9090090 // var scope is globle but its value and variable can be reassigned
+//using var is not a good pratice
+
+//datatypes
+
+//premititive datatypes
+// number => range from 2 to power 53
+// bigint => used for large number 
+// string => ""
+// boolean => true/false
+// Null => is a standalone value and it is also a datatype in javascritp
+// undefined => when value to a variable is not defined
+// null is the representation of an empty value which can also me defined to a variable
+// symbol => used to find the components in react unique components
+
+//object
+//null is an object whereas undefined is undefines which can be seen with typeof undefined/null
